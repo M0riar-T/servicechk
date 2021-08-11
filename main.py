9bf96fe985ab712f5450304b2a726cd1926d1a6d
M0riar-T <88714945+M0riar-T@users.noreply.github.com>
2021-08-11 23:25:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import sendanemail as mail
 import chk_service_with_email as cse
 
 cs.chk_service('sshd','ssh_service_check.log','service sshd start')
-out = cse.chk_service('freeswitch','/var/log/fs_chk.log','/usr/local/freeswitch/bin/freeswitch -nc -core')
+out = cse.chk_service('freeswitch','/var/log/fs_chk.log','/usr/local/freeswitch/bin/freeswitch -nc -core','receiver@gmail.com')
 print(out)
 sentmail = mail.sendanemail("dark.pearl007@gmail.com","The service status was", "/home/jenish/PycharmProjects/my_modules/456.log")
