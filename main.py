9cebf7302070da143c431780287ef05997627d64
M0riar-T <88714945+M0riar-T@users.noreply.github.com>
2021-08-11 23:18:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import chk_service as cs
-
 import sendanemail as mail
 import chk_service_with_email as cse
 
-# cs.chk_service('sshd','123.log','service sshd status')
-out = cse.chk_service('freeswitch','456.log','/usr/local/freeswitch/bin/freeswitch -nc -core')
+cs.chk_service('sshd','ssh_service_check.log','service sshd start')
+out = cse.chk_service('freeswitch','/var/log/fs_chk.log','/usr/local/freeswitch/bin/freeswitch -nc -core')
 print(out)
-# sentmail = mail.sendanemail("dark.pearl007@gmail.com","The service status was", "/home/jenish/PycharmProjects/my_modules/456.log")
+sentmail = mail.sendanemail("dark.pearl007@gmail.com","The service status was", "/home/jenish/PycharmProjects/my_modules/456.log")
